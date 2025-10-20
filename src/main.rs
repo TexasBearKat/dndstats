f57f0c8ca5c022e06de2eaed968b7bc5d5cebe48
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2025-10-20 18:02:54 -0500

    added the stuff
    yep

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use rand::Rng;
+
+fn main() {
+    
+}
+
+fn roll_stat_block() -> Vec<i32> {
+
+}
+
+fn roll_stat() -> i32 {
+    let rolls: Vec<i32> = Vec::new();
+
+    loop {
+        if (rolls.len() == 4) {
+            
+        }
+    }
+}
